39beaa941da5cbeb8586f80c1557f976de1b6360
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-22 18:35:34 -0500

    Brew formula update for prion version v0.3.1

--- Formula/prion.rb
+++ Formula/prion.rb
@@ -2,20 +2,20 @@
 class Prion < Formula
   desc "A vim package manager, designed for use with pathogen"
   homepage "https://github.com/Jefferson-Faseler/prion"
-  version "0.3.0"
+  version "0.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Jefferson-Faseler/prion/releases/download/v0.3.0/prion_0.3.0_Darwin_x86_64.tar.gz"
-    sha256 "3aec35f4af844126bb6b7095f5e4cbeb2ffe4b9596549577404ce724eafbc90d"
+    url "https://github.com/Jefferson-Faseler/prion/releases/download/v0.3.1/prion_0.3.1_Darwin_x86_64.tar.gz"
+    sha256 "049e7ffe099f633918739ec4c70e26b33421221c6e4837b100852495c558769b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Jefferson-Faseler/prion/releases/download/v0.3.0/prion_0.3.0_Linux_x86_64.tar.gz"
-      sha256 "69178d5d31a391621eae726ac1356bbb08f32ae84209ff16628f2d36aef5c964"
+      url "https://github.com/Jefferson-Faseler/prion/releases/download/v0.3.1/prion_0.3.1_Linux_x86_64.tar.gz"
+      sha256 "9a969cd3c11a6a68bf22d7db8cbc2691f0e49078ee27ded6352fe888bfeaf9bc"
     end
   end
-
-  depends_on "git" => "2.22.2"
+  
+  depends_on "git"
 
   def install
     bin.install "prion"
